db343ca8faa5afd690c1965b33bf2985dde23d19
brian <sx2618@nyu.edu>
2026-07-14 17:20:17 -0400

    implement rms norm module

--- cs336_basics/modules.py
+++ cs336_basics/modules.py
@@ -1,7 +1,6 @@
 # Linear Module for Transformer
 import torch
 import torch.nn as nn
-import torch.nn.Parameter as P
 import numpy as np
 from einops import rearrange, einsum, reduce
 
@@ -9,7 +8,7 @@ class LinearModule(nn.Module):
     def __init__(self, in_features:int, out_features:int, device: torch.device |None = None, dtype: torch.dtype |None = None):
         super().__init__()
         std = np.sqrt(2 / (in_features + out_features))
-        self.weight = P(torch.empty(out_features, in_features, device=device, dtype=dtype))
+        self.weight = nn.Parameter(torch.empty(out_features, in_features, device=device, dtype=dtype))
         nn.init.trunc_normal_(self.weight, std=std, a=-3*std, b=3*std)
         
 
@@ -26,7 +25,7 @@ class EmbeddingModule(nn.Module):
     def __init__(self, num_embeddings:int, embedding_dim:int, device: torch.device |None = None, dtype: torch.dtype |None = None):
         super().__init__()
         std = 1.0
-        self.embedding = P(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        self.embedding = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
         nn.init.trunc_normal_(self.embedding, std=std, a=-3*std, b=3*std)
         
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
@@ -35,3 +34,20 @@ class EmbeddingModule(nn.Module):
         # output: (batch, seq_len, embedding_dim)
         x = self.embedding[token_ids]
         return x
+
+
+class RMSLayerNorm(nn.Module):
+    def __init__(self, d_model:int, eps:float = 1e-5, device: torch.device |None = None, dtype: torch.dtype |None = None):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (batch, seq_len, d_model)
+        # output: (batch, seq_len, d_model)
+        in_type = x.dtype
+        x = x.to(torch.float32)
+        mean_square = reduce(x ** 2, "b s d -> b s 1", "mean")
+        x_norm = x / torch.sqrt(mean_square + self.eps)
+        result = x_norm * self.weight
+        return result.to(in_type)
--- tests/adapters.py
+++ tests/adapters.py
@@ -384,7 +384,10 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    from cs336_basics.modules import RMSNormModule
+    rmsnorm = RMSNormModule(d_model=d_model, eps=eps, device=in_features.device, dtype=in_features.dtype)
+    rmsnorm.load_state_dict({"weight": weights.to(device=in_features.device, dtype=in_features.dtype)})
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
